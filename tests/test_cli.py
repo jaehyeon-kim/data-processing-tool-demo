16dc32c1077223e895f6823d58493558c616e8ec
Jaehyeon Kim <dottami@gmail.com>
2021-08-05 07:54:27 +1000

    remove print

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,6 +6,5 @@ import cli
 def test_search():
     runner = CliRunner()
     results = runner.invoke(cli.search, ["--path", ".", "--ftype", "md"])
-    print(results.output)
     assert results.exit_code == 0
     assert ".md" in results.output
